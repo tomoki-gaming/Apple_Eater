0a0ec6802ec86e83a4ce9c02acc8f548684f0b07
tomoki-gaming <kuronosuarudo@gmail.com>
2024-03-28 19:35:46 +0900

    bug

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+phina.globalize();
+//画像アセット
+var ASSETS = {
+    image: {
+      'apple':'../Image/apple.png',
+      'bomb':'../Image/bomb.png'
+    },
+  };
+//シーン管理
+var myScenes = [
+    {
+      label: 'Main',
+      className: 'MainScene',
+      nextLabel: '',
+    },
+];
+//screen size
+let SCREEN_X = 640;
+let SCREEN_Y = 480;
+//diley_time
+let DIREY = 4000;
 //ビデオキャプチャ
 const player = document.getElementById('player');
 navigator.mediaDevices.getUserMedia({video:true, audio: false})
@@ -96,6 +117,44 @@ phina.define("MainScene", {
         }
     }
 });
+
+//リンゴ　クラス
+phina.define('Apple', {
+    superClass: 'Sprite',
+    init: function(image,x,y,object_name) {
+        this.superInit(image);
+        this.size = [50,50];
+        this.pad = 5;
+        this.object_name = object_name;
+        this.setSize(this.size[0],this.size[1]);
+        this.setPosition(x,y);
+    },
+    update: function() {
+        this.y +=1;
+    },
+    delete_in_mouse:function(pos){
+        if(SCREEN_X-this.x>=pos[0]+this.pad && SCREEN_X-this.x<=pos[1]-this.pad){
+            if(this.y>=pos[2]+this.pad && this.y<=pos[3]-this.pad){
+                this.remove();
+                if(this.object_name=='apple'){
+                    return 100;
+                }
+                else{
+                    return -100;
+                }
+            }
+        }
+        return 0;
+    },
+    delete_under_frame:function(){
+        if(this.y > SCREEN_Y-15){
+            this.remove();
+            return 1;
+        }
+        return 0;
+    },
+});
+
 //メイン関数
 phina.main(function() {
     var app = GameApp({startLabel: 'main', width: SCREEN_X, height: SCREEN_Y, assets: ASSETS});
